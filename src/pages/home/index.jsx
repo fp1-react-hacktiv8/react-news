3eceb95954560cf5be9d5fba51162700b22ce457
Ali Rosi <101112535+AliRosi@users.noreply.github.com>
2023-10-19 22:53:43 +0700

    membuat/menambahkan Card News

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/navbar/index.jsx";
 import Footer from "../../components/footer/index.jsx";
+import CardItem from "../../components/card_item/index.jsx";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <CardItem />
       <Footer />
     </div>
   );
