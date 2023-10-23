2f96a5c34902d09137a32e5542f3f7f9b21dbe29
YusronFauzanN <Yusronsronz7@gmail.com>
2023-10-23 11:20:16 +0700

    feature: add new page

---
+++ src/pages/covid/index.jsx
@@ -0,0 +1,15 @@
+import CardItem from "../../components/card_item";
+import Footer from "../../components/footer";
+import Navbar from "../../components/navbar";
+
+const Covid = () => {
+  return (
+    <>
+      <Navbar />
+      <CardItem />
+      <Footer />
+    </>
+  );
+};
+
+export default Covid;
---
+++ src/pages/search/index.jsx
@@ -0,0 +1,15 @@
+import CardItem from "../../components/card_item";
+import Footer from "../../components/footer";
+import Navbar from "../../components/navbar";
+
+const Search = () => {
+  return (
+    <>
+      <Navbar />
+      <CardItem />
+      <Footer />
+    </>
+  );
+};
+
+export default Search;
