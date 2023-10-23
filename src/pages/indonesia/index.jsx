e9ec829feaad109d6dda7078bb5e07ac61c2378a
YusronFauzanN <Yusronsronz7@gmail.com>
2023-10-23 11:19:12 +0700

    feature: add new pages

---
+++ src/pages/indonesia/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../components/navbar/index.jsx";
+import Footer from "../../components/footer/index.jsx";
+import CardItem from "../../components/card_item/index.jsx";
+
+const Indonesia = () => {
+  return (
+    <>
+      <Navbar />
+      <CardItem />
+      <Footer />
+    </>
+  );
+};
+
+export default Indonesia;
